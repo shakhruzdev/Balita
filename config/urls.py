a490d83408e10be3444981d65c50df8da4dc4f28
asus <sahruz708@gmail.com>
2024-06-21 20:04:15 +0500

    changed

--- config/urls.py
+++ config/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 
-    re_path(r'^static/(?P<path>.*)$', serve),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
