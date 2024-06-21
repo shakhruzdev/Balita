0d7a827bb8215f907bc0c15e6c41a4bf76c4b2ba
asus <sahruz708@gmail.com>
2024-06-21 19:42:42 +0500

    ...

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*lwi5574*m5u%=&@z5gtwk%5mp49&uwbxp4lp&e9%p53r(iky6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
